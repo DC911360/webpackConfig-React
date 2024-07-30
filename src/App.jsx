eb5b42c436f5dc47d34fcbeb6d24359d741fd205
DC911360 <15910609156@163.com>
2024-07-30 23:59:00 +0800

    打包 chunk 重命名

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import { Link, Routes, Route } from "react-router-dom";
 
 import { Button } from 'antd'
 
-const Home = lazy(() => import(/* webpackChunkName:'home' */"./page/Home"));
-const About = lazy(() => import(/* webpackChunkName:'about' */"./page/About"));
+// const Home = lazy(() => import(/* webpackChunkName:'home' */"./page/Home")); // chunkName:home
+// const About = lazy(() => import(/* webpackChunkName:'about' */"./page/About")); //chunkName: about
 
+/**
+ * 默认的 chunk 名称 : src_page_文件夹名称_文件名称_文件扩展名_chunk.js
+ */
+const Home = lazy(() => import("./page/Home")); // chunkName:[默认] src_page_Home_index_jsx.chunk.js  http://localhost:3000/static/js/src_page_About_index_jsx.chunk.js
+const About = lazy(() => import("./page/About")); //chunkName: [默认] src_page_About_index_jsx.chunk.js  http://localhost:3000/static/js/src_page_Home_index_jsx.chunk.js
 function App() {
   return (
     <div>
